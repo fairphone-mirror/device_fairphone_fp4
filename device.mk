cf685b106b7dc7d702b4d84d270b629e7e466282
Rajeshwar Kurapaty <rkurapat@codeaurora.org>
2020-11-09 11:29:22 +0530

    lito: Add netflix custom property in vendor
    Add lito specific netflix custom property
    for netflix whitelisting in vendor side.
    
    Change-Id: I391449735baee26cbeed21616237ac94747ee2df
    (cherry picked from commit 3375582660f3af2af4a7c69e500b66022682f068)

--- device.mk
+++ device.mk
@@ -798,6 +798,11 @@ PRODUCT_COPY_FILES += \
     $(FP_PATH)/configs/public.libraries.system_ext-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 
+# Target specific Netflix custom property
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q7250-19133-1
+
+
 # NFC
 $(call inherit-product, vendor/st/nfc/st21nfc/NfcDeviceConfig.mk)
 
