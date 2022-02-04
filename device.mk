94f0dc8a178099b00360d32401fbcaaac7db7fd1
Sal Savage <salsavage@google.com>
2022-02-04 09:41:46 -0800

    Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: I0a76275baf2c80894b6be71d79b3265021ac921b
    (cherry picked from commit 7a88659e6b8cdee704fc9168925cc7a421e22a92)

--- device.mk
+++ device.mk
@@ -283,9 +283,30 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.bluetooth.wipower=false
 
 # Bluetooth Properties
-# Hearing Aid Audio Support Using Bluetooth LE
+# Set supported Bluetooth profiles to enabled
 PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.profile.asha.central.enabled=true
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.avrcp.controller.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bap.broadcast.assist.enabled=true \
+    bluetooth.profile.bap.unicast.server.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.csip.set_coordinator.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hap.client.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.mcp.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.sap.server.enabled=true \
+    bluetooth.profile.tbs.server.enabled=true \
+    bluetooth.profile.vc.server.enabled=true
 
 
 # Board platforms lists to be used for
