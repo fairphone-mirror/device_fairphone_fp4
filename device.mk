44f16656f9533f7054312eb5350619dda5b28edc
dan.qian <dan.qian@t2mobile.com>
2023-07-24 20:45:09 +0530

    Initial changes for Fingerprint Bringup
    * Add Fingerprint HAL
    * Add Fingerprint permission file.
    
    Issue: FP4-INT#10
    Issue: FP4-149
    Change-Id: Iccda646c1685bf909c5523acc809dd47007ee05b
    (cherry picked from commit a341f4592144c04880b2f8caf0f4b3fa70237dfc)

--- device.mk
+++ device.mk
@@ -430,6 +430,14 @@ PRODUCT_PACKAGES += fastbootd
 PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
 
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+
 # Feature flags and Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
