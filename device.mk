39b1d48d44e62a2179347f450a024a5d2a5a71c4
Luca Weiss <luca.weiss@fairphone.com>
2023-08-10 15:38:11 +0200

    NFC bringup
    Include the Makefile adding all the necessary packages and add an entry
    in the manifest.xml.
    
    Add the necessary sepolicy files to that the NFC stack works.
    
    Issue: FP4-INT#20
    Issue: ALM:10277814
    Issue: ALM:10872985
    Change-Id: I06bf39afd09d085fe49add3ff67a1631d15355b3

--- device.mk
+++ device.mk
@@ -785,6 +785,11 @@ PRODUCT_COPY_FILES += \
 
 
 # NFC
+$(call inherit-product, vendor/st/nfc/st21nfc/NfcDeviceConfig.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.st_nfc_defaut_se=SIM1 \
+    ro.hardware.nfc_nci=pn54x
 
 
 # Oemaids
