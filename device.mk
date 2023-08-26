b118d38025b3bef4bd6a7c311e0fdd840714c166
Bharath <bharath@teamb58.org>
2023-08-26 22:50:20 +0530

    Initial changes for Camera Bringup
    * Add camera provider manifest entry.
    * Add camera hidl services.
    * Add all required modules.
    * Add other camera permissions.
    
    Issue: FP4-INT#9
    Change-Id: I6137445a940e36e0a0bc81dd4cf7fcc18b74a60d

--- device.mk
+++ device.mk
@@ -293,6 +293,25 @@ PRODUCT_COPY_FILES += \
 
 
 # Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-external \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-legacy \
+    android.hardware.camera.provider@2.4-service_64 \
+    camera.device@3.5-impl \
+    camera.device@3.6-external-impl \
+    libcamera2ndk_vendor \
+    libexif.vendor \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.postproc@1.0 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
+# Feature flags for camera
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 
 # Charger
