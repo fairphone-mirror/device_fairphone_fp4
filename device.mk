dcabb5fadb4b20b3352baf7a2dfbb69ce5ba8694
Bharath <bharath@teamb58.org>
2023-08-26 22:48:49 +0530

    Initial changes for Bluetooth Bringup
    * Add relevant Board flags for Bluetooth.
    * Add the manifest entry for bluetooth and bluetooth audio.
    * Add Bluetooth xml permissions.
    * Add Bluetooth HIDL services.
    * Import other Bluetooth configs.
    * Add ANT related modules.
    
    Issue: FP4-INT#7
    Change-Id: I78ff29e6e6f3abe621febc3c4358593fada15bc3

--- device.mk
+++ device.mk
@@ -214,7 +214,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    $(AUDIO_HAL_PATH)/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml \
+    $(AUDIO_HAL_PATH)/configs/common/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml
 
 # Audio Permissions
 PRODUCT_COPY_FILES += \
@@ -224,7 +226,40 @@ PRODUCT_COPY_FILES += \
 include $(FP_PATH)/audio_properties.mk
 
 
+# ANT
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0 \
+    com.dsi.ant@1.0.vendor
+
 # Bluetooth
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth@1.0 \
+    bt_stack.conf \
+    libbluetooth-binder \
+    libbluetooth_audio_session \
+    libchrome \
+    libchrome.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0 \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor
+
+# Bluetooth Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2 \
+    persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true \
+    persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=false \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.scram.enabled=false \
+    persist.vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false \
+    persist.vendor.service.bdroid.soc.alwayson=true \
+    ro.vendor.bluetooth.wipower=false
 
 
 # Board platforms lists to be used for
