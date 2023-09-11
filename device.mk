65e2c8aa97fe11f784ac0b80d9037c4d28f8e262
Bharath <bharath@teamb58.org>
2023-09-11 15:17:37 +0530

    Enable QCOM power pasr manager
    Issue: FP4-INT#19
    Change-Id: I2f0fa85ddc38f4e159f80054bcaf4b79db17e45d

--- device.mk
+++ device.mk
@@ -875,6 +875,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Pasr manager
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.power.pasr.enabled=true \
+    vendor.pasr.activemode.enabled=true
+
 
 # PPP
 PRODUCT_PACKAGES += \
