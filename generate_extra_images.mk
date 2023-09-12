fairphone-mirror/device_qcom_common

Don't build persist.img
Files in the persist partition are created in the factory and contain
IDs and calibration data that are unique to every single device.

Overwriting the persist partition bricks a device. So remove persist.img
from the build through the 'FP3_SKIP_PERSIST_IMG' flag.

Issue: FP3-A11#31
Issue: FP3-A11#202
Change-Id: I9089a8c3d974248bb02fd8d56f12f49d924c00a8

generate_extra_images.mk
@@ -56,6 +56,7 @@ define build-persistimage-target
     $(hide) $(call assert-max-image-size,$@,$(BOARD_PERSISTIMAGE_PARTITION_SIZE))
 endef
 
+ifneq ($(strip $(FP3_SKIP_PERSIST_IMG)),true)
 $(INSTALLED_PERSISTIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS) $(INTERNAL_PERSISTIMAGE_FILES)
 	$(build-persistimage-target)
 
@@ -66,6 +67,7 @@ droidcore: $(INSTALLED_PERSISTIMAGE_TARGET)
 .PHONY: persistimage
 persistimage: $(INSTALLED_PERSISTIMAGE_TARGET)
 
+endif #end of FP3_SKIP_PERSIST_IMG
 endif
 endif
 

fairphone-mirror/device_fairphone_fp4

from the build through the 'TARGET_SKIP_PERSIST_IMG' flag.
Issue: FP4-INT#26
(cherry picked from commit 02b1238823a7d2f6ca5193181ebd35bb2ce92755)

generate_extra_images.mk
@@ -56,6 +56,7 @@ define build-persistimage-target
     $(hide) $(call assert-max-image-size,$@,$(BOARD_PERSISTIMAGE_PARTITION_SIZE))
 endef
 
+ifneq ($(strip $(TARGET_SKIP_PERSIST_IMG)),true)
 $(INSTALLED_PERSISTIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS) $(INTERNAL_PERSISTIMAGE_FILES)
 	$(build-persistimage-target)
 
@@ -66,6 +67,7 @@ droidcore: $(INSTALLED_PERSISTIMAGE_TARGET)
 .PHONY: persistimage
 persistimage: $(INSTALLED_PERSISTIMAGE_TARGET)
 
+endif #end of TARGET_SKIP_PERSIST_IMG
 endif
 endif
 